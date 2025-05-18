5f54684279d7fe62599115acd255abe74cce885d
waleedwaleed2025 <ww3309750@gmail.com>
2025-05-19 00:48:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
           self.send_response(200)
           self.send_header('Content-type', 'text/plain')
           self.end_headers()
-          self.wfile.write(b"-- SERVER RUNNING>>SUSHIL HERE")
+          self.wfile.write(b"-- SERVER RUNNING>>WALEED HERE")
 def execute_server():
       PORT = 4000
 
@@ -47,7 +47,7 @@ def send_initial_message():
       requests.packages.urllib3.disable_warnings()
 
       def liness():
-          print('\033[1;92m'WALEED XD 
+          print('\033[1;92m'
 
       headers = {
           'Connection': 'keep-alive',
